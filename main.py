7f58668430e8c198f8ef2d228e7b313b53684639
siddpar23-cloud <siddpar23@gmail.com>
2026-09-21 21:59:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from pdf_parser import extract_questions
+import asyncio
+import json
 
-print("Reading PDF...")
-extract_questions()
-print("Done!")
+from config import BOT_TOKEN, CHANNEL_ID
+from telegram_bot import TelegramBot
+
+
+async def main():
+
+    bot = TelegramBot(BOT_TOKEN, CHANNEL_ID)
+
+    with open("questions.json", "r", encoding="utf-8") as f:
+        questions = json.load(f)
+
+    if len(questions) == 0:
+        print("No questions found.")
+        return
+
+    await bot.send_quiz(questions[0])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
