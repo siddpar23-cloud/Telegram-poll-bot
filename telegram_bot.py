5fa6c314144fe86e28b1bd82cd0dfc1c5504da0d
siddpar23-cloud <siddpar23@gmail.com>
2026-09-21 21:29:29 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,27 @@
 from telegram import Bot
+from telegram.constants import ParseMode
+import json
 
-class TelegramQuizBot:
-    def __init__(self, token):
-        self.bot = Bot(token=token)
+class TelegramBot:
 
-    async def send_message(self, chat_id, text):
-        await self.bot.send_message(
-            chat_id=chat_id,
-            text=text
+    def __init__(self, token, channel):
+        self.bot = Bot(token)
+        self.channel = channel
+
+    async def send_quiz(self, question):
+
+        await self.bot.send_poll(
+            chat_id=self.channel,
+            question=question["question"],
+            options=question["options"],
+            type="quiz",
+            correct_option_id=question["correct"],
+            is_anonymous=True
         )
+
+        if question.get("solution"):
+            await self.bot.send_message(
+                chat_id=self.channel,
+                text="📖 Explanation\n\n"+question["solution"],
+                parse_mode=ParseMode.HTML
+            )
