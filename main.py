3d903840fc2deeba90e7d0f0074ef78c852bf2ac
siddpar23-cloud <siddpar23@gmail.com>
2026-09-21 22:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import asyncio
 import json
 
-from config import BOT_TOKEN, CHANNEL_ID
+from config import BOT_TOKEN, CHANNEL_ID, QUESTIONS_PER_DAY
 from telegram_bot import TelegramBot
 
-
 async def main():
-
     bot = TelegramBot(BOT_TOKEN, CHANNEL_ID)
 
     with open("questions.json", "r", encoding="utf-8") as f:
         questions = json.load(f)
 
-    if len(questions) == 0:
+    if not questions:
         print("No questions found.")
         return
 
-    await bot.send_quiz(questions[0])
+    limit = min(QUESTIONS_PER_DAY, len(questions))
 
+    for i in range(limit):
+        await bot.send_quiz(questions[i])
+
+        if i != limit - 1:
+            await asyncio.sleep(45)
 
 if __name__ == "__main__":
     asyncio.run(main())
