fa8c12f536f931cdfab09858b33024be81b13178
siddpar23-cloud <siddpar23@gmail.com>
2026-09-21 21:21:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-import os
+from pdf_parser import extract_questions
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "✅ Protocol Bot is Online!\n\nDaily Quiz Polls will start after setup."
-    )
-
-app = Application.builder().token(BOT_TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-print("Bot Started...")
-app.run_polling()
+print("Reading PDF...")
+extract_questions()
+print("Done!")
