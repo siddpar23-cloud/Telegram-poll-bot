ed97e87ee527f8bde71b9f1465f3a56dd226363d
siddpar23-cloud <siddpar23@gmail.com>
2026-09-22 19:44:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import asyncio
 import json
+import threading
+
+from flask import Flask
 
 from config import BOT_TOKEN, CHANNEL_ID, QUESTIONS_PER_DAY
 from telegram_bot import TelegramBot
 
-async def main():
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+async def send_questions():
     bot = TelegramBot(BOT_TOKEN, CHANNEL_ID)
 
     with open("questions.json", "r", encoding="utf-8") as f:
@@ -16,11 +25,23 @@ async def main():
 
     limit = min(QUESTIONS_PER_DAY, len(questions))
 
-    for i in range(limit):
-        await bot.send_quiz(questions[i])
+    while True:
+        for i in range(limit):
+            await bot.send_quiz(questions[i])
 
-        if i != limit - 1:
-            await asyncio.sleep(45)
+            if i != limit - 1:
+                await asyncio.sleep(45)
+
+        print("Today's questions completed.")
+        await asyncio.sleep(86400)
+
+
+def start_bot():
+    asyncio.run(send_questions())
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    thread = threading.Thread(target=start_bot)
+    thread.start()
+
+    app.run(host="0.0.0.0", port=10000)
